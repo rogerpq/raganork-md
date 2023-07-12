7c47e538a37f04601d1797c607d9aab395cdbc2c
روجر <104784913+rogerpq@users.noreply.github.com>
2023-07-12 12:44:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -63,6 +63,7 @@ module.exports = {
     MODE: process.env.MODE || 'private',
     WARN: process.env.WARN || '4',
     ANTILINK_WARN: process.env.ANTILINK_WARN || ''
+    READ_COMMAND:
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY || '',
